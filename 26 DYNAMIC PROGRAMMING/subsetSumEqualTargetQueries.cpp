fed1efd6404d52df29a4f9f4dbda5d24409752ad
UllasDas004 <ullasdas110904@gmail.com>
2025-12-02 12:57:45 +0530

    DP printing with queries and caching

--- 26 DYNAMIC PROGRAMMING/subsetSumEqualTargetQueries.cpp
+++ 26 DYNAMIC PROGRAMMING/subsetSumEqualTargetQueries.cpp
@@ -8,7 +8,7 @@ but here are q queries also
 #include<bits/stdc++.h>
 using namespace std;
 int dp[101][10005];
-bool rec(vector<int>& a,int level,int sum,int t)
+bool rec(vector<int>& a,int level,int sum,int t,vector<int>& subset)
 {
     // base case
     if(level == a.size())
@@ -19,8 +19,13 @@ bool rec(vector<int>& a,int level,int sum,int t)
     //cache check
     if(dp[level][sum] != -1) return dp[level][sum];
     // transition
-    bool ans = rec(a,level+1,sum,t);
-    if(sum-a[level] >= 0) ans |= rec(a,level+1,sum-a[level],t);
+    bool ans = rec(a,level+1,sum,t,subset);
+    if(sum-a[level] >= 0)
+    {
+        bool flag = rec(a,level+1,sum-a[level],t,subset);
+        if(flag) subset.push_back(a[level]);
+        ans |= flag;
+    }
     return dp[level][sum] = ans;
 }
 int main()
@@ -39,8 +44,14 @@ int main()
         int t;
         cout<<"Enter the target sum:- ";
         cin>>t;
-        bool ans = rec(a,0,t,t);
-        if(ans) cout<<"Yes possible."<<endl;
+        vector<int> subset;
+        bool ans = rec(a,0,t,t,subset);
+        if(ans)
+        {
+            cout<<"Yes possible."<<endl;
+            for(int i=0;i<subset.size();i++) cout<<subset[i]<<" ";
+            cout<<endl;
+        }
         else cout<<"No not possible."<<endl;
     }
     return 0;
